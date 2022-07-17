9efe857ad7508270d4b7e108d8e07f905f50f344
Rizky Hendra Saputra <71587872+RizkyHendra@users.noreply.github.com>
2022-07-17 17:17:57 +0700

    push Inventory 1/4
    Progress Inventoty 1/4

---
+++ Assets/Script/Inventory/Inventory.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one instance of inventory");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnItemChanged();
+    // delegate void Delegasi adalah wadah untuk fungsi yang dapat digunakan sebagai variabel. Delegasi membantu dalam membuat kode modular dan efisien. Ini berfungsi seperti layanan berbasis langganan, di mana metode yang diteruskan dipanggil saat berlangganan dan sebaliknya.
+    public OnItemChanged onItemChangedCallBack;
+
+    private int space = 20;
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if(item.isDefaultItem)
+        {
+            if(items.Count >= space)
+            {
+                // make game message
+                Debug.Log("Not Enaogh room");
+                return false;
+            }
+            // message
+            items.Add(item);
+            if(onItemChangedCallBack  != null)
+            {
+                onItemChangedCallBack.Invoke();
+            }
+        }
+        return true;
+    }
+
+    public void Remove(Item item)
+       
+    {
+
+        items.Remove(item);
+        if(onItemChangedCallBack != null)
+        {
+            onItemChangedCallBack.Invoke();
+        }
+    }
+}
---
+++ Assets/Script/ItemPickUp.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Item item;
+    public GameObject parent;
+    private Button buttonPickUp;
+    void Start()
+    {
+        buttonPickUp = GameObject.FindGameObjectWithTag("PickUp").GetComponent<Button>();
+
+    }
+    void PickUp()
+    {
+        buttonPickUp.interactable = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            buttonPickUp.onClick.RemoveAllListeners();
+            buttonPickUp.onClick.AddListener(PickUp);
+            buttonPickUp.interactable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            buttonPickUp.onClick.RemoveListener(PickUp);
+            buttonPickUp.interactable = false;
+        }
+    }
+}
